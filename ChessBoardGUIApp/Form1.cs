cb0aa99aece1792640095c2530ccf2b9595b4c4c
pasanprem <46164432+pasanprem@users.noreply.github.com>
2020-07-26 14:39:58 +0100

    Add project files.

---
+++ ChessBoardGUIApp/Form1.cs
@@ -0,0 +1,62 @@
+﻿using ChessBoardModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessBoardGUIApp
+{   
+
+    public partial class Form1 : Form
+    {
+        //reference to the class board. containst the values of the board
+        static Board myBoard = new Board(8);
+
+        //2D array of buttons whose values are determinded by myBoard
+        public Button[,] btnGrid = new Button[myBoard.Size, myBoard.Size];
+
+        public Form1()
+        {
+            InitializeComponent();
+            populateGrid();
+        }
+
+        private void populateGrid()
+        {
+
+            int buttonSize = panel1.Width / myBoard.Size;
+
+            //make the panel a perfect square (not essential here)
+            panel1.Height = panel1.Width;
+
+            //nested loops. create buttons and print them to the screen
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    btnGrid[i, j] = new Button();
+
+                    btnGrid[i, j].Height = buttonSize;
+                    btnGrid[i, j].Width = buttonSize;
+
+                    //add click event
+                    btnGrid[i, j].Click += Grid_Button_Click;
+
+                    panel1.Controls.Add(btnGrid[i, j]);
+
+                    btnGrid[i, j].Location = new Point(i * buttonSize, j * buttonSize);
+                }
+            }
+        }
+
+        private void Grid_Button_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChessBoardModel/Board.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoardModel
+{
+    public class Board
+    {
+        //size of board, usually 8x8
+        public int Size { get; set; }
+
+        public Cell[,] theGrid { get; set; }
+
+        //constructor
+        public Board(int s)
+        {
+            //initial board size s
+            Size = s;
+
+            //cretae a new 2D array of Cells
+            theGrid = new Cell[Size, Size];
+
+            //fill 2D array with new Cells with unique x and y coordinates
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+
+        public void MarkNextLegalMoves(Cell currentCell, string chessPiece)
+        {
+            //step 1 - clear all previous legal movies
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j].LegalNextMove = false;
+                    theGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+
+            //step 2 - find all legal moves and mark the cells as 'legal'
+            switch(chessPiece)
+            {
+                case "Knight":
+                    if(isSafe(currentCell.RowNumber + 2, currentCell.ColumnNumber + 1))
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    if(isSafe(currentCell.RowNumber + 2, currentCell.ColumnNumber - 1))
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    if(isSafe(currentCell.RowNumber - 2, currentCell.ColumnNumber + 1))
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    if(isSafe(currentCell.RowNumber - 2, currentCell.ColumnNumber - 1))
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    if(isSafe(currentCell.RowNumber + 1, currentCell.ColumnNumber + 2))
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    if(isSafe(currentCell.RowNumber + 1, currentCell.ColumnNumber - 2))
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    if(isSafe(currentCell.RowNumber - 1, currentCell.ColumnNumber + 2))
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    if(isSafe(currentCell.RowNumber - 1, currentCell.ColumnNumber - 2))
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    break;
+
+                case "King":
+                    if (isSafe(currentCell.RowNumber + 1, currentCell.ColumnNumber + 1))
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    if (isSafe(currentCell.RowNumber + 1, currentCell.ColumnNumber + 0))
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 0].LegalNextMove = true;
+                    if (isSafe(currentCell.RowNumber + 1, currentCell.ColumnNumber - 1))
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    if (isSafe(currentCell.RowNumber + 0, currentCell.ColumnNumber - 1))
+                        theGrid[currentCell.RowNumber + 0, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    if (isSafe(currentCell.RowNumber + 0, currentCell.ColumnNumber + 1))
+                        theGrid[currentCell.RowNumber + 0, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    if (isSafe(currentCell.RowNumber - 1, currentCell.ColumnNumber + 1))
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    if (isSafe(currentCell.RowNumber - 1, currentCell.ColumnNumber + 0))
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 0].LegalNextMove = true;
+                    if (isSafe(currentCell.RowNumber - 1, currentCell.ColumnNumber - 1))
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    break;
+
+                case "Rook":
+                    for (int i = 1; i <= Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber - i, currentCell.ColumnNumber))
+                            theGrid[currentCell.RowNumber - i, currentCell.ColumnNumber].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber + i, currentCell.ColumnNumber))
+                            theGrid[currentCell.RowNumber + i, currentCell.ColumnNumber].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber, currentCell.ColumnNumber - i))
+                            theGrid[currentCell.RowNumber, currentCell.ColumnNumber - i].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber, currentCell.ColumnNumber + i))
+                            theGrid[currentCell.RowNumber, currentCell.ColumnNumber + i].LegalNextMove = true;
+                    }
+
+                    break;
+
+                case "Bishop":
+                    for (int i = 1; i <= Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber - i, currentCell.ColumnNumber - i))
+                            theGrid[currentCell.RowNumber - i, currentCell.ColumnNumber - i].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber + i, currentCell.ColumnNumber + i))
+                            theGrid[currentCell.RowNumber + i, currentCell.ColumnNumber + i].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber + i, currentCell.ColumnNumber - i))
+                            theGrid[currentCell.RowNumber + i, currentCell.ColumnNumber - i].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber - i, currentCell.ColumnNumber + i))
+                            theGrid[currentCell.RowNumber - i, currentCell.ColumnNumber + i].LegalNextMove = true;
+                    }
+                    break;
+
+                case "Queen":
+                    for (int i = 1; i <= Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber - i, currentCell.ColumnNumber))
+                            theGrid[currentCell.RowNumber - i, currentCell.ColumnNumber].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber + i, currentCell.ColumnNumber))
+                            theGrid[currentCell.RowNumber + i, currentCell.ColumnNumber].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber, currentCell.ColumnNumber - i))
+                            theGrid[currentCell.RowNumber, currentCell.ColumnNumber - i].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber, currentCell.ColumnNumber + i))
+                            theGrid[currentCell.RowNumber, currentCell.ColumnNumber + i].LegalNextMove = true;
+                    }
+                    for (int i = 1; i <= Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber - i, currentCell.ColumnNumber - i))
+                            theGrid[currentCell.RowNumber - i, currentCell.ColumnNumber - i].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber + i, currentCell.ColumnNumber + i))
+                            theGrid[currentCell.RowNumber + i, currentCell.ColumnNumber + i].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber + i, currentCell.ColumnNumber - i))
+                            theGrid[currentCell.RowNumber + i, currentCell.ColumnNumber - i].LegalNextMove = true;
+                    }
+                    for (int i = 0; i < Size; i++)
+                    {
+                        if (isSafe(currentCell.RowNumber - i, currentCell.ColumnNumber + i))
+                            theGrid[currentCell.RowNumber - i, currentCell.ColumnNumber + i].LegalNextMove = true;
+                    }
+                    break;
+
+                default:
+                    break;
+
+
+
+            }
+            theGrid[currentCell.RowNumber, currentCell.ColumnNumber].CurrentlyOccupied = true;
+        }
+
+        private bool isSafe(int rownum, int colnum)
+        {
+            if(0 <= rownum && rownum < Size)
+            {
+                if(0 <= colnum && colnum < Size)
+                {
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }    
+}
